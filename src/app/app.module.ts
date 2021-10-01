ce8ef259b530f661381e4f7b2c66afa24e0f05fb
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2021-10-01 17:03:02 +0800

    adding geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Storage],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Storage, Geolocation],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  latitude: any = 0;
+  longitude: any = 0;
+  constructor(public geolocation: Geolocation) { }
 
   ngOnInit() {
+  }
+
+  getCurrentCoordinates(){
+    this.geolocation.getCurrentPosition().then((data) => {
+      this.latitude = data.coords.latitude;
+      this.longitude = data.coords.longitude;
+    });
+
+    console.log(this.latitude);
+    console.log(this.longitude);
   }
 
 }
